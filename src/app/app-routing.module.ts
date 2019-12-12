8feeb934f34a4c489b138bbc70cb3c756e6b15b5
Nicolas-Liapis <nicolas.liapis.dev@gmail.com>
2019-12-12 15:36:20 +0200

    v0.4 - Added basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ForexComponent } from './tabs/forex/forex.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'forex', component: ForexComponent },
+  { path: '', redirectTo: 'forex', pathMatch: 'full'},
+  { path: '**', redirectTo: 'forex'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
